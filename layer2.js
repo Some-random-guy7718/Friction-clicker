56e48b82ef1d89d72ff24f91425a190e07064560
SomeRandomGuy7718 <40974907+SomeRandomGuy7718@users.noreply.github.com>
2022-01-18 18:17:25 -0600

    500% more nothing

--- layer2.js
+++ layer2.js
@@ -2,20 +2,36 @@
 var layer2 = {
     tableAmount: 0
 };
+var lastTab = ""
 // declare elements
+var tabContainer = document.createElement("div");
 var tableAmountText = document.createElement("div");
 var bt = document.querySelector("button");
 var resourcesHolder = document.createElement("div");
+var options = document.createElement("button");
+var resources = document.createElement("button");
+var stats = document.createElement("button");
 // change element css
-resourcesHolder.className = "genericContainer";
+options.textContent = "options";
+options.className = "tabButton"
+resources.textContent = "Resources";
+resources.className = "tabButton"
+stats.textContent = "Stats";
+stats.className = "tabButton"
+tabContainer.className = "tabContainer layer2 main"
+resourcesHolder.className = "genericContainer layer2 tab1";
 resourcesHolder.textContent = "Resources:";
 bt.style.display = "";
 bt.outerHTML = '<button id="bt", class="actionButton" onclick="layer2ClickFunction()">Get another table</button>';
-tableAmountText.className = "changingText";
+tableAmountText.className = "changingText main";
 tableAmountText.textContent = "You have " + layer2.tableAmount + " tables";
 // append elements
+document.body.appendChild(tabContainer)
 document.body.appendChild(tableAmountText);
 document.body.appendChild(resourcesHolder);
+tabContainer.appendChild(resources)
+tabContainer.appendChild(options)
+tabContainer.appendChild(stats)
 
 function layer2ClickFunction() {
     layer2.tableAmount += 1;
@@ -23,3 +39,5 @@ function layer2ClickFunction() {
     return;
 
 }
+
+
