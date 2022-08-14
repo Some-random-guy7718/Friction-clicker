c5b8701c8f0e9b4da1d4aa03bd31bdc241155cfb
SomeRandomGuy7718 <40974907+SomeRandomGuy7718@users.noreply.github.com>
2022-08-14 17:12:30 -0500

    Layer 2 now has More Content

--- layer2.js
+++ layer2.js
@@ -3,22 +3,38 @@ var layer2 = {
     tableAmount: 0,
     tablesForaged: 0,
     moneyAmount: 0,
+    morbAmount: 0,
+    activeMorbs: 0,
 };
+const time = parseInt(Date.now())
 var bt = document.getElementById(bt);
 var lastTab = 1;
-var incrementComplete = true;
+var incrementTableComplete = true;
+var incrementMorbComplete = true;
+var xAmount = 1;
+var effectHappening = 0;
 // unhide this layer
 document.querySelector('#layer2Wrapper').className = "";
 // declare elements
 var moneyAmountText = document.querySelector("#moneyAmountText");
 var tableAmountText = document.querySelector("#tableAmountText");
+var morbAmountText = document.querySelector("#morbAmountText");
 var bt = document.querySelector("button");
 var resourcesHolder = document.querySelector("#resourcesHolder");
+var statList = document.querySelector("#statList")
+var morbButton = document.querySelector("#morbUse")
 // change element css
 bt.style.display = "";
 bt.outerHTML = '<button id="bt", class="actionButton" onclick="layer2ClickFunction()">Forage for another table</button>';
 moneyAmountText.textContent = "You have " + layer2.moneyAmount + " dollars";
 tableAmountText.textContent = "You have " + layer2.tableAmount + " tables";
+morbAmountText.textContent = "You have " + layer2.morbAmount + " Morbs";
+function gameCycle(){
+    let timeElapsed = parseInt(Date.now()) - time
+    statList.textContent = "You have " + layer2.moneyAmount + " dollars" + br + "This will work someday" + "You have been playing for " + timeElapsed;
+    return;
+}
+let gameCycleVar = setInterval(gameCycle, 100)
 
 function layer2ClickFunction() {
     if (Math.random() > 2 ** (layer2.tablesForaged / 10) - 1) {
@@ -38,26 +54,81 @@ function tabLoad(t) {
     return lastTab = t;
 }
 
-function sell(what) {
-    if (layer2.tableAmount >= 1 && what == "table" && incrementComplete == true) {
+function shopInteract(int, what) {
+    if (layer2.tableAmount >= 1 && what == "table" && incrementTableComplete == true) {
         layer2.tableAmount -= 1;
         tableAmountText.textContent = "You have " + layer2.tableAmount + " tables";
-        incrementComplete = false;
+        incrementTableComplete = false;
         increment(0, "table");
     }
+    if (layer2.moneyAmount >= Math.floor(1 * 1.1 ** layer2.morbAmount) && what == 'morb' && incrementMorbComplete == true){
+        layer2.moneyAmount -= Math.floor(1 * 1.1 ** layer2.morbAmount)
+        moneyAmountText.textContent = "You have " + layer2.moneyAmount + " dollars";
+        incrementMorbComplete = false;
+        increment(0, "morb");
+        if(true == true){
 
+        }
+    }
+    return;
 }
 
 function increment(t, what) {
     if (what == "table" && t < 100) {
         t++;
         document.getElementById("sellTableFill").style.width = t + "%";
-        let incrementTimer = setTimeout(increment, 10, t, 'table');
+        let incrementTableTimer = setTimeout(increment, 10, t, 'table');
     } else if (t == 100) {
         document.getElementById("sellTableFill").style.width = 0 + "%";
         layer2.moneyAmount += 1;
         moneyAmountText.textContent = "You have " + layer2.moneyAmount + " dollars";
-        incrementComplete = true;
+        incrementTableComplete = true;
+    }
+    if (what == "morb" && t < 100) {
+        t++;
+        document.getElementById("buyMorbFill").style.width = t + "%";
+        let incrementMorbTimer = setTimeout(increment, 10, t, 'morb');
+    } else if (what == "morb" && t == 100) {
+        document.getElementById("buyMorbFill").style.width = 0 + "%";
+        layer2.moneyAmount -= 1;
+        layer2.morbAmount += 1;
+        moneyAmountText.textContent = "You have " + layer2.moneyAmount + " dollars";
+        morbAmountText.textContent = "You have " + layer2.morbAmount + " Morbs";
+        document.getElementById("bt2").classList.remove("hidden")
+        incrementMorbComplete = true;
     }
     return;
+}
+var randomSeed = Math.floor(Math.random() * 7);
+function morbInteract(int, what){
+    if (layer2.tablesForaged == 10 && layer2.tableAmount == 0 && layer2.moneyAmount == 0 && layer2.morbAmount == 0) {
+        console.log("Congratulations, you just softlocked yourself due to bad rng")
+        return;
+    }
+    if (layer2.morbAmount >= 1){
+        if (randomSeed > 4){
+            layer2.tableAmount += 1
+            tableAmountText.textContent = "You have " + layer2.tableAmount + " tables";
+            console.log('Your Morb "found" a table!')
+        } else if (randomSeed > 1) {
+            layer2.moneyAmount +=1
+            moneyAmountText.textContent = "You have " + layer2.moneyAmount + " dollars";
+            console.log("Your Morb stole someone's dollar!")
+        } else {
+            layer2.morbAmount -= 1
+            morbAmountText.textContent = "You have " + layer2.morbAmount + " Morbs";
+            console.log("Your Morb died horribly :(")
+        }
+    }
+    randomSeed = Math.floor(Math.random() * 7);
+    return;
+}
+
+
+function beautifulEffects(text){
+    if(effectHappening == 1){
+        // do something
+    } else {
+        // do something else
+    }
 }
